879b5c09190f3c5390485aed39e44ed5eeb3696e
Graziella Costa <100802546+grazie-lla@users.noreply.github.com>
2024-01-20 22:18:32 -0300

    modified class Main in exercicio3

--- src/main/java/br/com/ada/pooii/exercicio3/Main.java
+++ src/main/java/br/com/ada/pooii/exercicio3/Main.java
@@ -16,7 +16,7 @@ public class Main {
         var joao = new Pessoa(4, "João");
 
         System.out.println("Elemento na posição 2: " + listaDeNomes.obter(2)); // Saída: Pessoa[id=3, nome="jess]
-        System.out.println("A lista contém a pessoa maria? " + listaDeNomes.contem(joao)); // Saída: true
+        System.out.println("A lista contém a pessoa maria? " + listaDeNomes.contem(maria)); // Saída: true
         System.out.println("Qual a posicao do elemento de ID 3? " + listaDeNomes.indice(3L)); // Saída: 2
         System.out.println("A lista contém a pessoa João? " + listaDeNomes.contem(joao)); // Saída: false
     }
